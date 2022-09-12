dcf13384d52fec309f8767a85f6acecd7ae9cd4d
Frederick Cortez <frederickcortez66@gmail.com>
2022-09-12 12:24:02 -0500

    first commit

--- api/server.js
+++ api/server.js
@@ -1,3 +1,51 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
+const server = express();
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.post('/api/users', async (req, res) => {
+    try {
+        const {name, bio} = req.body
+        if (!name||!bio) {
+            res.status(400).json({
+                message: 'Please provide name and bio for the user'
+            })
+        } else {
+            const createUser = await User.insert({name, bio})
+            res.status(201).json({
+                message: 'Success creating user',
+                data: createUser
+            })
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: `There was an error while saving the user to the database`
+        })
+    }
+})
+
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch(err) {
+        res.status(500).json({
+            message: `The users information could not be retrieved`
+        })
+    }
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const user = await Users.findById(id)
+        res.status(200).json(user)
+    } catch(err) {
+        res.status(500).json({
+            message: 'The user information could not be retrieved'
+        })
+    }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
