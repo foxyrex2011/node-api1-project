bf57d652d9b965affbdca2e3051b664538787595
Frederick Cortez <frederickcortez66@gmail.com>
2022-09-12 13:50:34 -0500

    second commit

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express();
 server.use(express.json());
 
 server.post('/api/users', async (req, res) => {
-    try {
+    try{
         const {name, bio} = req.body
         if (!name||!bio) {
             res.status(400).json({
@@ -26,7 +26,7 @@ server.post('/api/users', async (req, res) => {
 })
 
 server.get('/api/users', async (req, res) => {
-    try {
+    try{
         const users = await User.find()
         res.status(200).json(users)
     } catch(err) {
@@ -37,13 +37,60 @@ server.get('/api/users', async (req, res) => {
 })
 
 server.get('/api/users/:id', async (req, res) => {
-    try {
+    try{
         const {id} = req.params
-        const user = await Users.findById(id)
-        res.status(200).json(user)
+        const user = await User.findById(id)
+        if (!user) {
+            res.status(404).json({message: `The user with the specified ID does not exist`})
+        } else {
+            res.status(200).json(user)
+        }
     } catch(err) {
         res.status(500).json({
             message: 'The user information could not be retrieved'
+        })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params
+        const deleteUser = await User.remove(id)
+        if (!deleteUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else {
+            res.json({
+                message: 'User deleted',
+                data: deleteUser
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The user could not be removed'
+        })
+    }
+})
+
+server.put('/api/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params
+        const {name, bio} = req.body
+        if (!name || !bio) {
+            res.status(400).json({
+                message:'Please provide name and bio for the user'
+            })
+        } else {
+            const updatedUser = await User.update(id, {name, bio})
+            res.status(200).json({
+            message: 'Success',
+            data: updatedUser
+            })
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: 'The user information could not be modified'
         })
     }
 })
